80d4beef42c5c0a405adee06e80c6b715fca486c
Pablo Serrano <pablo.s.toro@gmail.com>
2021-06-17 01:51:54 +0200

    Updates

---
+++ precache-manifest.f32382b8eb867a8132f6ab32da18576c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97205a2a3923a0d930991b90ccb790a6",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "c1848451258efe1f0f83",
+    "url": "/todo/static/css/main.e9cbf68a.chunk.css"
+  },
+  {
+    "revision": "d524544b42a5a7c5a06a",
+    "url": "/todo/static/js/2.c269e3d4.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/todo/static/js/2.c269e3d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1848451258efe1f0f83",
+    "url": "/todo/static/js/main.410c154e.chunk.js"
+  },
+  {
+    "revision": "da7c3f7f7e5963fe74b8",
+    "url": "/todo/static/js/runtime-main.74315f96.js"
+  }
+]);
